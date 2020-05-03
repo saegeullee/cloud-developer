e0a4512e34dde74bab7e130a7fcd5b4559493072
saegeullee <lemix777@naver.com>
2020-05-03 12:48:02 +0900

    done so far

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/models/FeedItem.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/models/FeedItem.ts
@@ -1,9 +1,21 @@
-import {Table, Column, Model, HasMany, PrimaryKey, CreatedAt, UpdatedAt, ForeignKey} from 'sequelize-typescript';
-import { User } from '../../users/models/User';
+import {
+  Table,
+  Column,
+  Model,
+  HasMany,
+  PrimaryKey,
+  CreatedAt,
+  UpdatedAt,
+  ForeignKey
+} from "sequelize-typescript";
+import { User } from "../../users/models/User";
 
 @Table
 export class FeedItem extends Model<FeedItem> {
   @Column
+
+  //Column decorator declares that there will be a caption column within our database. Sequelize is clever enough to be able to map the TypeScript type to the Postgres type.
+  //we can add a custom exclamation point to declare whether or not that kind of field can be null or not.
   public caption!: string;
 
   @Column
